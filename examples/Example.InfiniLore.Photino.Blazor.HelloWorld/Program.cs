4eef3428d08f2f3e6c3dac4586e100aba295df04
Anna Sas <anna.sas.dev@gmail.com>
2025-09-17 11:39:38 +0200

    Refactor namespace and directory structure to align with InfiniLore.Photino standards; update project files accordingly

---
+++ examples/Example.InfiniLore.Photino.Blazor.HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using InfiniLore.Photino.Blazor;
+
+namespace Example.InfiniLore.Photino.Blazor.HelloWorld
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+            appBuilder.Services
+                .AddLogging();
+
+            // register root component
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Hello World");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+        }
+
+    }
+}
---
+++ src/InfiniLore.Photino.Blazor/PhotinoBlazorAppConfiguration.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfiniLore.Photino.Blazor
+{
+    public class PhotinoBlazorAppConfiguration
+    {
+        public Uri AppBaseUri { get; set; }
+
+        public string HostPage { get; set; }
+    }
+}
