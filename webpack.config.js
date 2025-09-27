4188f7ebcc42de8fcb2a4b94a3a070042d58f482
Anna Sas <anna.sas.dev@gmail.com>
2025-09-27 17:51:32 +0200

    Feat: webpack production building during deployment to nuget

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 ﻿//webpack.config.js
 const path = require('path');
 
-module.exports = {
-    mode: "development",
-    // mode: "production",
-    devtool: "inline-source-map",
-    entry: {
-        main: "./src/InfiniLore.Photino.Js/TsSource/Index.ts",
-    },
-    output: {
-        path: path.resolve(__dirname, './src/InfiniLore.Photino.Js/wwwroot'),
-        filename: "InfiniLore.Photino.js" // <--- Will be compiled to this single file
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
+module.exports = (env, args) => {
+    const isProduction = env?.production === true;
+    
+    return {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? false : 'inline-source-map',
+        entry: {
+            main: "./src/InfiniLore.Photino.Js/TsSource/Index.ts",
+        },
+        output: {
+            path: path.resolve(__dirname, './src/InfiniLore.Photino.Js/wwwroot'),
+            filename: "InfiniLore.Photino.js", // <--- Will be compiled to this single file
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        }   
     }
 };
