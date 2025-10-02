cd146a3d3ca070a1fb23c696bcab05e5d6358ec9
Anna Sas <anna.sas.dev@gmail.com>
2025-10-02 18:13:51 +0200

    Refactor: Consolidate service registration logic into `PhotinoBlazorAppBuilder` and remove redundant `ServiceCollectionExtensions`.

--- src/InfiniLore.Photino.Blazor/PhotinoBlazorAppBuilder.cs
+++ src/InfiniLore.Photino.Blazor/PhotinoBlazorAppBuilder.cs
@@ -3,11 +3,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
 namespace InfiniLore.Photino.Blazor;
+using InfiniLore.Photino.Js;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
 public class PhotinoBlazorAppBuilder {
     public RootComponentList RootComponents { get; } = new RootComponentList();
     public IServiceCollection Services { get; } = new ServiceCollection();
     public IPhotinoWindowBuilder WindowBuilder { get; } = PhotinoWindowBuilder.Create();
-
+    
+    private PhotinoBlazorAppBuilder() {}
+    
     public static PhotinoBlazorAppBuilder CreateDefault(string[]? args = null, Action<IPhotinoWindowBuilder>? windowBuilder = null) {
         return CreateDefault(null, args, windowBuilder);
     }
@@ -16,14 +22,28 @@ public class PhotinoBlazorAppBuilder {
         // We don't use the args for anything right now, but we want to accept them
         // here so that it shows up this way in the project templates.
         var appBuilder = new PhotinoBlazorAppBuilder();
-        appBuilder.Services.AddPhotinoBlazorDesktop(fileProvider: fileProvider, windowBuilder: windowBuilder);
-        appBuilder.Services.AddSingleton(appBuilder.WindowBuilder);
-        appBuilder.Services.AddSingleton(appBuilder.RootComponents);
 
-        // Right now we don't have conventions or behaviors that are specific to this method
-        // however, making this the default for the template allows us to add things like that
-        // in the future, while giving `new BlazorDesktopHostBuilder` as an opt-out of opinionated
-        // settings.
+        appBuilder.Services.AddOptions<PhotinoBlazorAppConfiguration>();
+        
+        appBuilder.Services
+            .AddSingleton(fileProvider ?? new PhysicalFileProvider(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot")))
+            .AddScoped(static sp => {
+                var handler = sp.GetRequiredService<PhotinoHttpHandler>();
+                return new HttpClient(handler) { BaseAddress = new Uri(PhotinoWebViewManager.AppBaseUri) };
+            })
+            .AddScoped<IInfiniWindowJs, InfiniWindowJs>()
+            .AddSingleton<IPhotinoWebViewManager, PhotinoWebViewManager>()
+            .AddSingleton<IPhotinoJsComponentConfiguration, PhotinoJsComponentConfiguration>()
+            .AddSingleton<Dispatcher, PhotinoDispatcher>()
+            .AddSingleton<JSComponentConfigurationStore>()
+            .AddSingleton<PhotinoBlazorApp>()
+            .AddSingleton<PhotinoHttpHandler>()
+            .AddSingleton<PhotinoSynchronizationContext>()
+            .AddSingleton<IPhotinoWindow>(static provider => provider.GetRequiredService<IPhotinoWindowBuilder>().Build(provider))
+            .AddBlazorWebView()
+            .AddSingleton(appBuilder.WindowBuilder)
+            .AddSingleton(appBuilder.RootComponents);
+
         return appBuilder;
     }
 
@@ -43,7 +63,7 @@ public class PhotinoBlazorAppBuilder {
         AppDomain.CurrentDomain.UnhandledException += (_, error) => {
             sp.GetService<IPhotinoWindow>()?.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
         };
-        
+
         return sp.GetRequiredService<PhotinoBlazorApp>();
     }
 }
