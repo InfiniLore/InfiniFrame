58db112732dde9646610ad53b888d35084e8d9fb
Anna Sas <anna.sas.dev@gmail.com>
2025-10-02 17:36:36 +0200

    Feat: Add `sendMessageToHost` method to `IInfiniWindow` and implement it for improved host messaging support

--- src/InfiniLore.Photino.Js/TsSource/Contracts/IInfiniWindow.ts
+++ src/InfiniLore.Photino.Js/TsSource/Contracts/IInfiniWindow.ts
@@ -1,13 +1,15 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-import {IHostMessaging} from "./IHostMessaging";
+import {IHostMessaging, SendToHostMessageId} from "./IHostMessaging";
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 export interface IInfiniWindow {
     HostMessaging : IHostMessaging;
+
+    sendMessageToHost(id: SendToHostMessageId, data?: string): void;
     
     setPointerCapture(element: Element, pointerId: number): void;
     releasePointerCapture(element: Element, pointerId: number): void;
--- src/InfiniLore.Photino.Js/TsSource/InfiniWindow.ts
+++ src/InfiniLore.Photino.Js/TsSource/InfiniWindow.ts
@@ -2,13 +2,18 @@
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
 import {IInfiniWindow} from "./Contracts/IInfiniWindow";
-import {IHostMessaging} from "./Contracts/IHostMessaging";
+import {IHostMessaging, SendToHostMessageId} from "./Contracts/IHostMessaging";
 import {HostMessaging} from "./HostMessaging";
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 export class InfiniWindow implements IInfiniWindow {
     HostMessaging : IHostMessaging = new HostMessaging();
+
+    // Overload to make a dev's life easier instead of having to go to the HostMessaging class
+    sendMessageToHost(id: SendToHostMessageId, data?: string) {
+        this.HostMessaging.sendMessageToHost(id, data);
+    }
     
     setPointerCapture(element:Element, pointerId: number) {
         element.setPointerCapture(pointerId);
