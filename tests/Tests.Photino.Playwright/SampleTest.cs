3d2e2e92a595bf5fb6b88b3bc2e4d62cc9cb3d2b
Anna Sas <anna.sas.dev@gmail.com>
2025-10-08 14:44:12 +0200

    Refactor: Migrate test utilities to `Shared.Photino`, update namespaces, and enhance test organization. Add Playwright test project for browser-based integration.

---
+++ tests/Tests.Photino.Playwright/SampleTest.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.Photino.NET;
+using Tests.Shared.Photino;
+using TUnit.Playwright;
+using InfiniLore.Photino.NET.Server;
+using Microsoft.Playwright;
+
+namespace Tests.Photino.Playwright;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class SampleTest : PageTest {
+    public override string BrowserName { get; } = "webkit";
+
+    [Test]
+    [STAThread]
+    public async Task Test1() {
+        // Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
+        
+        // Arrange
+        using var utility = WindowServerTestUtility.Create(
+            serverBuilder => serverBuilder
+                .UsePort(9000),
+            
+            windowBuilder => windowBuilder
+                .SetBrowserControlInitParameters("--remote-debugging-port=9222")
+        );
+        
+        IBrowser browser = await Playwright.Chromium.ConnectOverCDPAsync("http://127.0.0.1:9222");
+        IBrowserContext context = browser.Contexts[0];
+        IPage page = context.Pages[0];
+
+        var title = await page.TitleAsync();
+        await Assert.That(title).IsEqualTo("Photino Playwright Test");
+        
+        
+        // IResponse? r = await Page.GotoAsync("http://localhost:9222/json");
+        // string content = await (r?.TextAsync() ?? Task.FromResult(string.Empty));
+        // await Assert.That(content).IsNotEmpty();
+        //
+        // IResponse? response = await Page.GotoAsync("http://localhost:9000/");
+        // string title = await Page.TitleAsync();
+        //
+        // // Assert
+        // await Assert.That(response).IsNotNull();        
+        // await Assert.That(response?.Status).IsEqualTo(200);
+        //
+        
+        
+        // windowServer.Dispose();
+    }
+}
