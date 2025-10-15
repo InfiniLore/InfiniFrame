949ee90474d85ef281808649c57557211e3af2a0
Anna Sas <anna.sas.dev@gmail.com>
2025-10-15 15:13:51 +0200

    Update InfiniWindowNative.cs

--- src/InfiniLore.Photino.Shared/Native/InfiniWindowNative.cs
+++ src/InfiniLore.Photino.Shared/Native/InfiniWindowNative.cs
@@ -9,8 +9,16 @@ namespace InfiniLore.Photino.Native;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public static class InfiniWindowNative {
-    [DllImport(DllName, EntryPoint = InfiniWindowTests_NativeParametersReturnAsIs, CallingConvention = CallingConvention.Cdecl, SetLastError = true, CharSet = CharSet.Ansi)]
+    #pragma warning disable SYSLIB1054
+    [DllImport(
+        DllName,
+        EntryPoint = InfiniWindowTests_NativeParametersReturnAsIs,
+        CallingConvention = CallingConvention.Cdecl,
+        SetLastError = true,
+        CharSet = CharSet.Ansi
+    )]
     private static extern void NativeParametersReturnAsIs(ref PhotinoNativeParameters parameters, out IntPtr newParameters);
+    #pragma warning restore SYSLIB1054
 
     internal static PhotinoNativeParameters NativeParametersReturnAsIs(ref PhotinoNativeParameters parameters) {
         NativeParametersReturnAsIs(ref parameters, out IntPtr newParameters);
