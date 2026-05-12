5ad72bdba467c02e338588f4d3a52b5d97cbd61c
Anna Sas <anna.sas.dev@gmail.com>
2026-05-12 13:36:35 +0200

    Update Dockerfiles to use Node.js 24 and adjust Windows Dockerfile to fetch Node.js dynamically

--- docker/scripts/docker-windows-compose.ps1
+++ docker/scripts/docker-windows-compose.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $composeFile = Join-Path $scriptDir "..\compose\infiniframe-windows.yml"
+$env:DOCKER_BUILDKIT = "0"
+$env:COMPOSE_DOCKER_CLI_BUILD = "0"
 
 docker compose -f $composeFile build --no-cache `
     windows-tests `
