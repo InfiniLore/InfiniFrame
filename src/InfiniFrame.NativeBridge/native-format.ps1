47f7e056ef5c0a5dc38a445984f34b8dd7d3868e
Anna Sas <anna.sas.dev@gmail.com>
2026-05-17 21:45:47 +0200

    Add `native-tidy.ps1` script and update `native-format.ps1` to exclude dependencies and packages directories.

--- src/InfiniFrame.NativeBridge/native-format.ps1
+++ src/InfiniFrame.NativeBridge/native-format.ps1
@@ -1,4 +1,9 @@
-﻿Get-ChildItem -Recurse -Include *.cpp,*.cxx,*.cc,*.c,*.hpp,*.hh,*.hxx,*.h,*.ixx,*.mm,*.m |
+﻿Get-ChildItem -Recurse -File -Include *.cpp,*.cxx,*.cc,*.c,*.hpp,*.hh,*.hxx,*.h,*.ixx,*.mm,*.m |
+Where-Object {
+    $_.FullName -notmatch '\\Dependencies\\' -and
+        $_.FullName -notmatch '\\packages\\'
+} |
 ForEach-Object {
+    Write-Host "Formatting $($_.FullName)"
     clang-format -i $_.FullName
 }
---
+++ src/InfiniFrame.NativeBridge/native-tidy.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem . -Recurse -File -Include *.cpp,*.cc,*.cxx,*.c,*.hpp,*.h |
+Where-Object {
+    $_.FullName -notmatch '\\(build|out|bin|obj|vcpkg)\\'
+} |
+ForEach-Object {
+    Write-Host "Running clang-tidy on $($_.FullName)"
+    clang-tidy $_.FullName -p build --fix
+}
