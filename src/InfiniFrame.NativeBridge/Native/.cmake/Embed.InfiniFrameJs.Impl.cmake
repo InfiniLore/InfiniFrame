a51975cd999f7f2856bf9885f2e34bd7b0c89754
Anna Sas <anna.sas.dev@gmail.com>
2026-05-19 09:21:28 +0200

    Improve `Embed.InfiniFrameJs` handling: add auto-generation metadata, adjust includes, and update alignment logic.

--- src/InfiniFrame.NativeBridge/Native/.cmake/Embed.InfiniFrameJs.Impl.cmake
+++ src/InfiniFrame.NativeBridge/Native/.cmake/Embed.InfiniFrameJs.Impl.cmake
@@ -19,7 +19,12 @@ endforeach()
 string(TIMESTAMP GENERATED_AT "%Y-%m-%d %H:%M:%S UTC" UTC)
 
 # Header file
-file(WRITE "${OUTPUT_HEADER}" "#pragma once
+file(WRITE "${OUTPUT_HEADER}" "// -----------------------------------------------------------------------------
+// Auto-generated file. Do not edit manually.
+// Generated at: ${GENERATED_AT}
+// -----------------------------------------------------------------------------
+#pragma once
+
 // ReSharper disable once CppUnusedIncludeDirective
 #include <cstddef>
 
@@ -28,12 +33,12 @@ extern const size_t GInfiniframeJsSize;
 ")
 
 # Source file
-file(WRITE "${OUTPUT_SOURCE}" "#include \"InfiniFrameJs.h\"
-
-// -----------------------------------------------------------------------------
+file(WRITE "${OUTPUT_SOURCE}" "// -----------------------------------------------------------------------------
 // Auto-generated file. Do not edit manually.
 // Generated at: ${GENERATED_AT}
 // -----------------------------------------------------------------------------
+
+#include \"Embedded/InfiniFrameJs/InfiniFrameJs.h\"
 
 alignas(16) const unsigned char GInfiniframeJsData[] = {${BYTES}};
 
--- src/InfiniFrame.NativeBridge/Native/Embedded/InfiniFrameJs/InfiniFrameJs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// ReSharper disable once CppUnusedIncludeDirective
-#include <cstddef>
-
-extern const unsigned char GInfiniframeJsData[]; // NOLINT(*-avoid-c-arrays)
-extern const size_t GInfiniframeJsSize;
