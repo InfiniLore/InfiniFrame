a86bb61dd886bdf425d284b48f1cd713b18bd63c
Anna Sas <anna.sas.dev@gmail.com>
2026-05-21 15:50:52 +0200

    Add Linux skip attribute to `WindowClosedEventTests` for CI reliability

--- tests/InfiniFrameTests/WindowEvents/WindowClosedEventTests.cs
+++ tests/InfiniFrameTests/WindowEvents/WindowClosedEventTests.cs
@@ -12,6 +12,7 @@ public class WindowClosedEventTests {
     [Test]
     [Retry(5)]
     [SkipUtility.SkipOnMacOs]
+    [SkipUtility.SkipOnLinux]
     [NotInParallel(ParallelControl.InfiniFrame)]
     public async Task TestWindowClosedEvent(CancellationToken ct = default) {
         // Arrange
