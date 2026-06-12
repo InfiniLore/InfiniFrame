0361b64a6a5c0466cf5e92934633145901a27c7e
Anna Sas <anna.sas.dev@gmail.com>
2026-06-12 17:12:04 +0200

    Expansions of testing

---
+++ tests/InfiniTests.InfiniFrame/Window/Features/InfiniFrameWindowFeatureBrowser/JavascriptClipboardAccessTests.cs
@@ -0,0 +1,97 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniFrame;
+using InfiniFrame.NativeBridge.Parameters;
+
+namespace InfiniTests.InfiniFrame.Window.Features.InfiniFrameWindowFeatureBrowser;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class JavascriptClipboardAccessTests {
+
+    [Test]
+    [Arguments(true)]
+    [Arguments(false)]
+    public async Task AtBuilderStage_DirectAssignment(bool value, CancellationToken ct) {
+        // Arrange
+        var builder = InfiniFrameWindowBuilder.Create();
+        
+        // Act
+        builder.Features.Browser.EnableJavascriptClipboardAccess(value);
+        InfiniFrameNativeParameters initParameters = builder.CollectNativeParameters();
+
+        // Assert   
+        await Assert.That(builder.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+        await Assert.That(initParameters.JavascriptClipboardAccessEnabled).IsEqualTo(value);
+    }
+    
+    [Test]
+    [Arguments(true)]
+    [Arguments(false)]
+    public async Task AtBuilderStage_ExtensionAssignment(bool value, CancellationToken ct) {
+        // Arrange
+        var builder = InfiniFrameWindowBuilder.Create();
+        
+        // Act
+        IInfiniFrameWindowBuilder returnedBuilder = builder.EnableJavascriptClipboardAccess(value);
+        InfiniFrameNativeParameters initParameters = builder.CollectNativeParameters();
+
+        // Assert   
+        await Assert.That(builder.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+        await Assert.That(returnedBuilder).IsSameReferenceAs(builder);
+        await Assert.That(initParameters.JavascriptClipboardAccessEnabled).IsEqualTo(value);
+    }
+    
+    // [Test]
+    // [NotInParallelInfiniTests]
+    // [Arguments(true)]
+    // [Arguments(false)]
+    // public async Task AtWindowStage_DirectAssignment(bool value, CancellationToken ct) {
+    //     // Arrange
+    //     using var windowUtility = InfiniFrameTestWindow.Create(ct);
+    //     IInfiniFrameWindow window = windowUtility.Window;
+    //
+    //     // Act
+    //     window.Features.Browser.EnableJavascriptClipboardAccess(value);
+    //
+    //     // Assert
+    //     await Assert.That(window.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+    // }
+    //
+    // [Test]
+    // [NotInParallelInfiniTests]
+    // [Arguments(true)]
+    // [Arguments(false)]
+    // public async Task AtWindowStage_ExtensionAssignment(bool value, CancellationToken ct) {
+    //     // Arrange
+    //     using var windowUtility = InfiniFrameTestWindow.Create(ct);
+    //     IInfiniFrameWindow window = windowUtility.Window;
+    //
+    //     // Act
+    //     IInfiniFrameWindow returnedWindow = window.EnableJavascriptClipboardAccess(value);
+    //
+    //     // Assert
+    //     await Assert.That(window.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+    //     await Assert.That(returnedWindow).IsSameReferenceAs(window);
+    // }
+    
+    [Test]
+    [NotInParallelInfiniTests]
+    [Arguments(true)]
+    [Arguments(false)]
+    public async Task AtWindowStage_ThroughBuilderAssignment(bool value, CancellationToken ct) {
+        // Arrange
+        using var windowUtility = InfiniFrameTestWindow.Create(builder => {
+            builder.Features.Browser.EnableJavascriptClipboardAccess(value);
+        }, ct);
+        IInfiniFrameWindow window = windowUtility.Window;
+        IInfiniFrameWindowBuilder builder = windowUtility.BuilderSnapshot;
+
+        // Act
+
+        // Assert
+        await Assert.That(builder.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+        await Assert.That(window.Features.Browser.IsJavascriptClipboardAccessEnabled).IsEqualTo(value);
+    }
+}
