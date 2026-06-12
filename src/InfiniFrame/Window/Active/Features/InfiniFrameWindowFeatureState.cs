de4c57377eb974aff47148ea52b3ddccb693db80
Anna Sas <anna.sas.dev@gmail.com>
2026-06-12 13:49:37 +0200

    Refactor `InfiniFrameWindowFeatureState`: Remove redundant code after conditional block

--- src/InfiniFrame/Window/Active/Features/InfiniFrameWindowFeatureState.cs
+++ src/InfiniFrame/Window/Active/Features/InfiniFrameWindowFeatureState.cs
@@ -284,6 +284,8 @@ public class InfiniFrameWindowFeatureState(
                     currentMonitorArea.Height
                 );
             }
+
+            return;
         }
 
         // Set Fullscreen to false => Restore to previous state
