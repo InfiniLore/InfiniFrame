4a3d900bc79e7547cb59950880a38581f22e03f5
Anna Sas <anna.sas.dev@gmail.com>
2026-08-01 19:02:15 +0200

    a

--- src/InfiniFrame.NativeBridge/Managed/NativeApi/Exports/InfiniFrameNative.Lifecycle.cs
+++ src/InfiniFrame.NativeBridge/Managed/NativeApi/Exports/InfiniFrameNative.Lifecycle.cs
@@ -4,7 +4,6 @@
 using InfiniFrame.NativeBridge.Parameters;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.Marshalling;
 
 namespace InfiniFrame.NativeBridge;
 // ---------------------------------------------------------------------------------------------------------------------
@@ -19,7 +18,27 @@ public partial class InfiniFrameNative {
     /// <returns>A status code indicating success or failure.</returns>
     [LibraryImport(ArtifactManifest.NativeLibraryName, EntryPoint = "InfiniFrameNative_ctor", SetLastError = true)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial InfiniFrameNativeInteropStatus Constructor([MarshalUsing(typeof(InfiniFrameNativeParametersMarshaller))] in InfiniFrameNativeParameters parameters, out IntPtr value);
+    private static partial InfiniFrameNativeInteropStatus ConstructorNative(IntPtr parameters, out IntPtr value);
+
+    internal static InfiniFrameNativeInteropStatus Constructor(
+        in InfiniFrameNativeParameters parameters,
+        out IntPtr value
+    ) {
+        var marshaller = new InfiniFrameNativeParametersMarshaller.ManagedToUnmanagedIn();
+        marshaller.FromManaged(parameters);
+        InfiniFrameNativeParametersMarshaller.Unmanaged unmanaged = marshaller.ToUnmanaged();
+        IntPtr unmanagedPtr = IntPtr.Zero;
+
+        try {
+            unmanagedPtr = Marshal.AllocHGlobal(Marshal.SizeOf<InfiniFrameNativeParametersMarshaller.Unmanaged>());
+            Marshal.StructureToPtr(unmanaged, unmanagedPtr, false);
+            return ConstructorNative(unmanagedPtr, out value);
+        }
+        finally {
+            if (unmanagedPtr != IntPtr.Zero) Marshal.FreeHGlobal(unmanagedPtr);
+            marshaller.Free();
+        }
+    }
 
     /// <summary>
     ///     Destroys the native window instance and releases its resources.
